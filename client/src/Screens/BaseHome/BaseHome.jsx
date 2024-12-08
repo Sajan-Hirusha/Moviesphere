4aa0becf03e35cb73c25bf29dde7d8589a126715
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2024-12-08 13:41:25 +0530

    Upload Folder Structure

--- client/src/Screens/BaseHome/BaseHome.jsx
+++ client/src/Screens/BaseHome/BaseHome.jsx
@@ -1,8 +1,11 @@
-function BaseHome(){
-    return(
-        <div className="BaseHome">
+import Header from "../../Components/Header/Header.jsx";
 
+function BaseHome() {
+    return (
+        <div className="BaseHome">
+            <Header/>
         </div>
     )
 }
+
 export default BaseHome;
