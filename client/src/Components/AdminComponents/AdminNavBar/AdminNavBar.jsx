8c797bbfe31c0a9c184d38aaabe24124371c6154
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2024-12-14 01:39:23 +0530

    developing dashboard

--- client/src/Components/AdminComponents/AdminNavBar/AdminNavBar.jsx
+++ client/src/Components/AdminComponents/AdminNavBar/AdminNavBar.jsx
@@ -1,3 +1,4 @@
+import logo from "../../../assets/Images/siteLogo.png"
 function AdminNavBar() {
     return (
         <div className="adminNavBar mb-5">
@@ -16,14 +17,7 @@ function AdminNavBar() {
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <a className="navbar-brand mt-2 mt-lg-0" href="/">
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transparent-noshadows.webp"
-                                height="15"
-                                alt="MDB Logo"
-                                loading="lazy"
-                            />
-                        </a>
+
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link text-white" href="/dashboard">Dashboard</a>
@@ -41,65 +35,15 @@ function AdminNavBar() {
                             <i className="fas fa-shopping-cart"></i>
                         </a>
 
-                        <div className="dropdown">
-                            <a
-                                className="text-reset me-3 dropdown-toggle hidden-arrow text-white"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-mdb-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <i className="fas fa-bell"></i>
-                                <span className="badge rounded-pill badge-notification bg-danger">1</span>
-                            </a>
-                            <ul
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">Some news</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Another news</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Something else here</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="dropdown">
-                            <a
-                                className="dropdown-toggle d-flex align-items-center hidden-arrow text-white"
-                                href="#"
-                                id="navbarDropdownMenuAvatar"
-                                role="button"
-                                data-mdb-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <img
-                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                    className="rounded-circle"
-                                    height="25"
-                                    alt="Avatar"
-                                    loading="lazy"
-                                />
-                            </a>
-                            <ul
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="navbarDropdownMenuAvatar"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">My profile</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Settings</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Logout</a>
-                                </li>
-                            </ul>
-                        </div>
+
+                        <a className="navbar-brand mt-2 mt-lg-0" href="/">
+                            <img
+                                src={logo}
+                                alt="Logo"
+                                loading="lazy"
+                                style={{width: "110px", height: "40px"}}
+                            />
+                        </a>
                     </div>
                 </div>
             </nav>
